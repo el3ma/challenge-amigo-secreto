24042c519d0470ecb99de75b2f763db2e50b5426
Emanuel Astudillo <astudillo.emanuel@gmail.com>
2025-01-20 22:54:36 -0300

    función de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo() {
+    if (document.getElementById('amigo').value === '') {
+        alert('Debes escribir un nombre');
+        return;
+    } else {
+
+        let amigo = document.getElementById('amigo').value;
+        let listaAmigos = document.getElementById('listaAmigos');
+        nombresAmigos.push(amigo);
+        document.getElementById('amigo').value = ''
+        listaAmigos.innerHTML = `${nombresAmigos.map(amigo => `<li>${amigo}</li>`).join('')}`;
+
+        console.log(nombresAmigos);
+    }
+}
